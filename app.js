cfebadab36f60273dae7f200b1f5350c15a9e776
janko12 <henrich.sury97@gmail.com>
2020-09-26 20:55:43 +0200

    reinforced security

--- app.js
+++ app.js
@@ -12,8 +12,7 @@ var helmet = require('helmet');
 var app = express();
 var mongoose = require("mongoose");
 
-var dev_db_url = "mongodb+srv://janko12:istvanko@cluster0.firq3.mongodb.net/local_library?retryWrites=true&w=majority";
-var mongoDB = process.env.MONGODB_URI || dev_db_url;
+var mongoDB = process.env.MONGODB_URI;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 var db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
